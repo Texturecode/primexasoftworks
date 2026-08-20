a3f709a8387b6a294fcad6de19b4e902fe0d74bd
Muhammad Ubaid <muhammadubaid8@gmail.com>
2026-08-20 09:11:08 +0500

    Fixing analytics issue

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,7 +1,7 @@
 import { Fragment } from 'react'
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from '@vercel/analytics/react';
 import { Tagline } from '../components/Tagline'
 import { Navbar } from '../components/Navbar'
 import { Footer } from '../components/Footer'
@@ -9,11 +9,11 @@ import { Footer } from '../components/Footer'
 const RootLayout = () => (
   <Fragment className="">
     <Tagline />
+    <Analytics />
     <Navbar />
     <Outlet />
     <Footer />
     <TanStackRouterDevtools />
-    <Analytics />
   </Fragment>
 )
 
