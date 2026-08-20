f53dcd85715832e8e37a0d568ebb0a627a792298
Muhammad Ubaid <muhammadubaid8@gmail.com>
2026-08-20 09:09:53 +0500

    Added vercel analytics

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { Analytics } from "@vercel/analytics/next"
 import { Tagline } from '../components/Tagline'
 import { Navbar } from '../components/Navbar'
 import { Footer } from '../components/Footer'
@@ -12,6 +13,7 @@ const RootLayout = () => (
     <Outlet />
     <Footer />
     <TanStackRouterDevtools />
+    <Analytics />
   </Fragment>
 )
 
